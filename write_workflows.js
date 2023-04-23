b828a059314e7ecacd0ccf39d98e028e538bc675
Robert Konrad <git2016@robdangero.us>
2023-04-23 13:40:19 +0200

    Work on the workflow-script

--- write_workflows.js
+++ write_workflows.js
@@ -7,13 +7,14 @@ const samples = [
 	'MultiWindow',
 	'ComputeShader',
 	'TextureArray',
-	'ShaderG5'
+	'ShaderG5',
+  'RuntimeShaderCompilation'
 ];
 
 const workflowsDir = path.join('.github', 'workflows');
 
-let workflow =
-`name: Linux (Vulkan)
+function writeWorkflow(workflow) {
+  let workflowText = `name: ${workflow.sys} (${workflow.gfx})
 
 on:
   push:
@@ -30,7 +31,54 @@ jobs:
 
     steps:
     - uses: actions/checkout@v2
-    - name: Get LunarG key
+${workflow.steps}
+    - name: Get Submodules
+      run: ./get_dlc
+`;
+
+  for (const sample of samples) {
+    if (sample === 'RuntimeShaderCompilation') {
+      if (!workflow.RuntimeShaderCompilation) {
+        continue;
+      }
+    }
+    workflowText +=
+`    - name: Compile ${sample}
+      working-directory: ${sample}
+      run: ../Kinc/make ${workflow.sys.toLowerCase()} -g ${workflow.gfx.toLowerCase()} --compile
+`;
+  }
+
+  fs.writeFileSync(path.join(workflowsDir, workflow.sys.toLowerCase() + '-' + workflow.gfx.toLowerCase() + '.yml'), workflowText, {encoding: 'utf8'});
+}
+
+const workflows = [
+  {
+    sys: 'Android',
+    gfx: 'OpenGL',
+    steps: ''
+  },
+  {
+    sys: 'Android',
+    gfx: 'Vulkan',
+    steps: ''
+  },
+  {
+    sys: 'Linux',
+    gfx: 'OpenGL',
+    steps:
+`    - name: Apt Update
+      run: sudo apt update
+    - name: Apt Install
+      run: sudo apt-get install libasound2-dev libxinerama-dev libxrandr-dev libgl1-mesa-dev libxi-dev libxcursor-dev libudev-dev libwayland-dev wayland-protocols libxkbcommon-dev ninja-build --yes --quiet
+`,
+    RuntimeShaderCompilation: true
+  },
+  {
+    sys: 'Linux',
+    gfx: 'Vulkan',
+    steps:
+`    - name: Get LunarG key
       run: wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
     - name: Get LunarG apt sources
       run: sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-jammy.list http://packages.lunarg.com/vulkan/lunarg-vulkan-jammy.list
@@ -38,16 +86,10 @@ jobs:
       run: sudo apt update
     - name: Apt Install
       run: sudo apt install libasound2-dev libxinerama-dev libxrandr-dev libgl1-mesa-dev libxi-dev libxcursor-dev libudev-dev vulkan-sdk libwayland-dev wayland-protocols libxkbcommon-dev ninja-build --yes --quiet
-    - name: Get Submodules
-      run: ./get_dlc
-`;
+`
+  }
+];
 
-for (const sample of samples) {
-	workflow +=
-`    - name: Compile ${sample}
-      working-directory: ${sample}
-      run: ../Kinc/make -g vulkan --compile
-`;
+for (const workflow of workflows) {
+  writeWorkflow(workflow);
 }
-
-fs.writeFileSync(path.join(workflowsDir, 'linux-vulkan.yml'), workflow, {encoding: 'utf8'});
