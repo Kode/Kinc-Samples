e8b6ea27e666c94820ea5e1d3f32bb391971ed5f
Robert Konrad <git2016@robdangero.us>
2023-10-04 22:55:50 +0200

    Optimize the get_dlc scripts

--- get_dlc.bat
+++ get_dlc.bat
@@ -1,3 +1,3 @@
-@git -C %~dp0 submodule update --init
-@git -C %~dp0\RuntimeShaderCompilation submodule update --init --recursive
+@git -C %~dp0 submodule update --init Kinc
+@git -C %~dp0 submodule update --init --recursive RuntimeShaderCompilation/krafix
 @call %~dp0\Kinc\get_dlc.bat
